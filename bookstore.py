71b8b30d5dfbbd645febfdbba6b6e828406bade3
LDO <ldo@tut.by>
2018-04-23 16:58:08 +0300

    new

--- bookstore.py
+++ bookstore.py
@@ -2,6 +2,36 @@
 
 """
 from tkinter import *
+import backend
+
+def view_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
+        list1.insert(END, row)
+
+def add_command():
+    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    view_command()
+    
+def update_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+def delete_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+def close_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END, row)
 
 window = Tk()
 
@@ -41,6 +71,26 @@ sb1.grid(row=2, column=2, rowspan=6)
 
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
+
+
+
+b1 = Button(window, text='View all', width=12, command=view_command)
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text='Search entry', width=12, command=search_command)
+b2.grid(row=3, column=3)
+
+b3 = Button(window, text='Add entry', width=12, command=add_command)
+b3.grid(row=4, column=3)
+
+b4 = Button(window, text='Update', width=12, command=update_command)
+b4.grid(row=5, column=3)
+
+b5 = Button(window, text='Delete', width=12, command=delete_command)
+b5.grid(row=6, column=3)
+
+b6 = Button(window, text='Close', width=12, command=close_command)
+b6.grid(row=7, column=3)
 
 
 window.mainloop()
